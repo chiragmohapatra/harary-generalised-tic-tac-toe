964134bdb9baf899ecd2b0d4a7aa1c444e7ab559
chiragmohapatra <chirag131020@gmail.com>
2020-06-25 12:54:24 +0530

    updated

--- src/includes/game.hpp
+++ src/includes/game.hpp
@@ -9,6 +9,8 @@
 #define player 'x'
 #define opponent 'o'
 
+extern int polyamino_type; // 0 for Tippy , 1 for Fatty and 2 for Tic
+
 class Game{
 public:
   virtual ~Game() {};
@@ -21,10 +23,12 @@ public:
   virtual bool isTerminal() = 0; //returns true for a terminal position(player 1 win / player 1 lose / draw)
   virtual int evaluate() = 0; // return +10 for player 1 win , -10 for loss/draw and 0 for intermediate
   virtual bool isValidMove(int,int) const = 0; // returns true if move made is valid
-  virtual void make_move(bool,int,int) = 0;
+  virtual void make_move(int,int) = 0;
   virtual void undo_move(int,int) = 0; // undo the move at (int,int)
   virtual void print_board() = 0;
   virtual std::string print_as_string() const = 0;
+  virtual std::vector<std::string> generateChildren() = 0;
+  virtual bool isPlayer() = 0; // returns true if next move is of the player
 };
 
 #endif
